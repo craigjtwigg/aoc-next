9d7d9b592b736cc3bd153eb8e6a4e06a68d8de56
craigjtwigg <81586983+craigjtwigg@users.noreply.github.com>
2021-11-27 21:16:31 +0000

    gallery centered and all assets imported

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,22 +1,25 @@
 import { ImageList, ImageListItem } from '@mui/material';
 import { galleryData } from '../data';
 import Image from 'next/image';
-//import styles from '../styles/Gallery.module.css'
+import styles from '../styles/Gallery.module.css'
 
 export const Gallery = (props) => {
   return (
-    <div className="gallery">
-      <ImageList variant="masonry" cols={3} gap={8}>
+    <div className={styles.gallery}>
+      <ImageList variant="masonry" cols={3} gap={10}>
         {props.activeGallery.map((item) => (
           <ImageListItem key={item.imgSrc}>
+              <div style={{ position: 'relative', paddingBottom: '1vw', width: '30vw', height: 'auto' }}>
             <Image
              // onClick={() => setActiveImage(item)}
               src={item.imgSrc}
               alt={item.alt}
-            //  objectFit="contain"
-             layout='intrinsic'
-             quality='50'
+              objectFit="contain"
+              quality='40'
+              placeholder="blur"
+              blurDataURL={item.blurSrc ? item.blurSrc : undefined}
             />
+            </div>
           </ImageListItem>
         ))}
       </ImageList>
