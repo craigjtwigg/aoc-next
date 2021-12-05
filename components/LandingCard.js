c40daa91d65b3050540eca04bb628de9122f8327
craigjtwigg <81586983+craigjtwigg@users.noreply.github.com>
2021-12-05 23:53:50 +0000

    mobile view landing page redesigned

--- components/LandingCard.js
+++ components/LandingCard.js
@@ -1,33 +1,63 @@
-import React from 'react'
-import Image from 'next/dist/client/image'
-import styles from '../styles/Landing.module.css'
-import Link from 'next/link'
+import React from 'react';
+import Image from 'next/dist/client/image';
+import styles from '../styles/Landing.module.css';
+import Link from 'next/link';
 
 const LandingCard = (props) => {
-    return (
-        <Link href={props.link}>
-            <a className={styles.cardLink}>
-                   <div className={styles.card}>
-      <div className={styles.titleWrap}>
-        <h2 className={styles.title}>{props.category}</h2>
-      </div>
-      
-      <div className={styles.imageWrap}>
-      <Image
-        src={props.cardImage}
-        alt={`${props.category} work`}
-        layout='fill'
-        objectFit='cover'
-        quality="100"
-        placeholder="blur"
-        className={styles.cardImage}
-      />
-      </div>
-    </div>
-            </a>
-        </Link>
-      
-    )
-}
+  return (
+    <>
+    <LandingCardMobile mobileImage={props.mobileImage} link={props.link} category={props.category} cardImage={props.cardImage}/>
+    <Link href={props.link}>
+      <a className={styles.cardLink}>
+        <div className={styles.card}>
+          <div className={styles.titleWrap}>
+            <h2 className={styles.title}>{props.category}</h2>
+          </div>
 
-export default LandingCard
+          <div className={styles.imageWrap}>
+            <Image
+              src={props.cardImage}
+              alt={`${props.category} work`}
+              layout="fill"
+              objectFit="cover"
+              quality="100"
+              placeholder="blur"
+              className={styles.cardImage}
+            />
+          </div>
+        </div>
+      </a>
+    </Link>
+    </>
+  );
+};
+
+export const LandingCardMobile = (props) => {
+  return (
+    
+    <Link href={props.link}>
+      <a className={styles.cardLinkMobile}>
+        <div className={styles.card}>
+          <div className={styles.titleWrapMobile}>
+            <h2 className={styles.titleMobile}>{props.category}</h2>
+          </div>
+
+          <div className={styles.imageWrapMobile}>
+            <Image
+              src={props.mobileImage}
+              alt={`${props.category} work`}
+              layout="intrinsic"
+              objectFit="cover"
+              quality="100"
+              placeholder="blur"
+              className={styles.cardImageMobile}
+            />
+          </div>
+        </div>
+      </a>
+    </Link>
+  
+  );
+};
+
+export default LandingCard;
--- components/LandingMobile.js
+++ components/LandingMobile.js
@@ -3,20 +3,18 @@ import styles from '../styles/Landing.module.css';
 import CelebImg from '../public/assets/celebrity/kiri_pritchard_mcclean_03.jpg';
 import EditorialImg from '../public/assets/editorial/NO FUTURE MAGAZINE/no_future_02.jpg';
 import FilmAndTVImg from '../public/assets/tv/live_at_aberyswyth_pier_11.jpg';
+import CommercialImgMobile from '../public/assets/commercial/KOI FOOTWEAR/koi_footwear_01.jpg'
 import CommercialImg from '../public/assets/commercial/oxfam_01.jpg';
-import CommercialImgMobile from '../public/assets/commercial/KOI FOOTWEAR/koi_footwear_02.jpg'
 import LandingCard from './LandingCard';
-import LandingMobile from './LandingMobile';
 import { InstaTab, EmailTab } from './Tab';
 
 
-export const Landing = (props) => {
+const LandingMobile = (props) => {
   return (
     <>
-    <LandingMobile />
-    <div className={styles.container}>
-      <div className={styles.subContainer}>
-         <h1 className={styles.vertical}>ALEX OGDEN CLARK</h1>
+    <div className={styles.containerMobile}>
+      <div className={styles.subContainerMobile}>
+         <h1 className={styles.nameMobile}>ALEX OGDEN CLARK</h1>
        
 <LandingCard link='/editorial' category='Editorial' mobileImage ={EditorialImg} cardImage={EditorialImg}/>
 <LandingCard link='/tv' category='Film and TV' mobileImage ={FilmAndTVImg} cardImage={FilmAndTVImg}/>
@@ -29,6 +27,9 @@ export const Landing = (props) => {
   <EmailTab />
   <InstaTab />
 </div>
+
 </>
   )
 };
+
+export default LandingMobile
