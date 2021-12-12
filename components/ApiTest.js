f21d05de944695f88cae10ee2097917b7d370b3f
craigjtwigg <81586983+craigjtwigg@users.noreply.github.com>
2021-12-12 17:14:08 +0000

    images succesfully pulled and rendered from strapi cms locally

--- components/ApiTest.js
+++ components/ApiTest.js
@@ -8,69 +8,46 @@ export const ApiTest = () => {
     redirect: 'follow',
   };
 
-  const [apiImage, setApiImage] = useState(DefaultImage);
-  const [imageWidth, setImageWidth] = useState('500px');
-  const [imageHeight, setImageHeight] = useState('500px');
-  const [photoSet, setPhotoSet] = useState (null)
-
-
+  const [galleryData, setGalleryData] = useState(null);
 
   useEffect(() => {
-      fetch(
+    fetch(
       'http://localhost:1337/api/photo-sets?populate[images][fields]=url,width,height',
       requestOptions
     )
       .then((response) => response.json())
       .then((result) => {
-        let imgData = result.data[0].attributes.images.data
-        let imgUrl =
-          'http://localhost:1337' +
-          result.data[0].attributes.images.data[0].attributes.url;
-        let imgWidth =
-          result.data[0].attributes.images.data[0].attributes.width;
-        let imgHeight =
-          result.data[0].attributes.images.data[0].attributes.height;
-        imgUrl = imgUrl.toString();
-        //imgWidth = imgWidth.toString();
-        //imgHeight = imgHeight.toString()
-        setImageHeight(imgHeight);
-        setImageWidth(imgWidth);
-        setApiImage(imgUrl);
-        setPhotoSet(imgData)
-        //console.log(imgData)
-        //console.log(imgUrl);
+        setGalleryData(result.data);
       })
       .catch((error) => console.log('error', error));
     return () => {};
-  });
+  }, []);
 
-  return photoSet ? photoSet.map((item) => {
-  let itemUrl = 'http://localhost:1337' + item.attributes.url
+  return (galleryData ? (
+    galleryData.map((album) => {
+      let albumName = album.attributes.name;
+      let albumDescription = album.attributes.description;
+      let albumCredit = album.attributes.credit;
+      let albumCategory = album.attributes.albumCategory;
+       return (album.attributes.images.data.map((item) => (
+        <div key={'http://localhost:1337' + item.attributes.url}>
+          <Image
+            src={'http://localhost:1337' + item.attributes.url}
+            alt={albumDescription}
+            width={item.attributes.width}
+            height={item.attributes.height}
+            layout="responsive"
+            data-credit={albumCredit}
+            data-category={albumCategory}
+            data-photoset={albumName}
+            quality='100'
+          />
+        </div> )
+       ) )
+    }
+    )
+  )
+  : null
+  )
+}
 
-return    (      <div>
-      <Image
-        src={itemUrl}
-        width={item.attributes.width}
-        height={item.attributes.height}
-        layout='intrinsic'
-        alt="whatever"
-        quality='100'
-      />
-    </div>)
-    })
-
-    :
-  
-  (
-    <div>
-      <Image
-        src={apiImage}
-        width={imageWidth}
-        height={imageHeight}
-        layout='intrinsic'
-        alt="whatever"
-        quality='100'
-      />
-    </div>
-  );
-};
